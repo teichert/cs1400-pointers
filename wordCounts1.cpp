154d7e622d75c94dcd782c77519ed24e57542f42
Adam Teichert <adamteichert@gmail.com>
2020-04-15 09:27:13 -0600

    initial file with TODO's

---
+++ wordCounts1.cpp
@@ -0,0 +1,16 @@
+// Purpose: Demonstrate pointers and C-Strings in the C++ by reading
+// words from a text file, sorting them, and then printing word counts
+// without using <string>.  (In this version, we also do not use
+// <cstring>)
+
+// Author: Adam Teichert and Snow College CS 1400 class Spring 2020
+// Date: Started April 15, 2020
+
+// TODO: test and write compareCStrings
+// TODO: test and write copyCString
+// TODO: test and write nextWord
+// TODO: test and write readWords
+// TODO: test and write freeWords
+// TODO: test and write sortWords
+// TODO: test and write countWords
+// TODO: write main
