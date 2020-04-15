2bff48f4ed657828f01b4ae056df428764f5ca18
Adam Teichert <adamteichert@gmail.com>
2020-04-15 10:24:03 -0600

    tested and implemented stringLength

--- wordCounts1.cpp
+++ wordCounts1.cpp
@@ -6,6 +6,13 @@
 // Author: Adam Teichert and Snow College CS 1400 class Spring 2020
 // Date: Started April 15, 2020
 
+
+#include <cassert>
+#include <iostream>
+using std::cout;
+using std::endl;
+
+// TODO: test and write stringLength
 // TODO: test and write compareCStrings
 // TODO: test and write copyCString
 // TODO: test and write nextWord
@@ -15,6 +22,34 @@
 // TODO: test and write countWords
 // TODO: write main
 
+// return the length of the given c-string
+int stringLength(const char* s);
+
+void testDriver() {
+    assert(stringLength("") == 0);
+    assert(stringLength("hi") == 2);
+    assert(stringLength(" ") == 1);
+    assert(stringLength("0") == 1);
+    assert(stringLength("I") == 1);
+    assert(stringLength("hello world") == 11);
+    char str[] = {'h', 'i', '\0'};
+    assert(stringLength(str) == 2);
+    const char* str2 = "hi";
+    assert(stringLength(str2) == 2);
+    cout << "All tests passed." << endl;
+}
+
+int stringLength(const char* s) {
+    int length = 0;
+    // increment length until we find the null terminating character
+    for (int i = 0; s[i] != '\0'; i++) {
+        length++;
+    }
+    return length;
+}
+
+
 int main() {
+    testDriver();
     return 0;
 }
