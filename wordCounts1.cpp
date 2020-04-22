d527156283fcc6b51fdb89b5f5b4851400ec52d8
Adam Teichert <adamteichert@gmail.com>
2020-04-22 10:28:45 -0600

    strcpy and nextWord (nextWord isn't quite right because cin >> nextChar skips space already; need to use cin.get())

--- wordCounts1.cpp
+++ wordCounts1.cpp
@@ -9,19 +9,24 @@
 #include <cassert> // <assert.h>
 #include <cstring> // <string.h> strlen
 #include <iostream>
+#include <sstream> // istringstream
+using std::istringstream; // looks like cin, but we can hard code what gets typed
 using std::cout;
 using std::endl;
 
 // xTODO: test and write stringLength (strlen)
 // xTODO: test and write compareCStrings (strcmp)
 // xTODO: test and write compareCStrings2 ('B' < 'b' < 'C' < 'c')
-// TODO: test and write copyCString (strcpy)
-// TODO: test and write nextWord (cin >> word)
+// xTODO: test and write copyCString (strcpy)
+// TODO: test and write nextWord (cin >> word) using new keyword and talk about memory leaks
 // TODO: test and write readWords
 // TODO: test and write freeWords
 // TODO: test and write sortWords
 // TODO: test and write countWords
 // TODO: write main
+
+// copy the contents of the src c-string to dest
+void copyCString(char* dest, const char* src);
 
 // return true is 'a' points to the same string of
 // characters as 'b' (you can strcmp from <cstring>)
@@ -49,6 +54,10 @@ int compareCStrings(const char* lhs, const char* rhs);
 // return positive number if lhs should come later than rhs
 // return 0 if lhs is the same as rhs
 int compareCStrings2(const char* lhs, const char* rhs);
+
+// return a newly allocated c-string containing the next word (up to whitespace)
+// from the given input stream (or returns nullptr if there are no more words to read)
+char* nextWord(std::istream& input);
 
 // test our intcmp function
 void test_intcmp();
@@ -100,7 +109,6 @@ void testDriver() {
         const char* str2 = "hi";
         assert(stringLength(str2) == 2);
     }
-
     { // implemented in <cstring> as strlen
         assert(strlen("") == 0);
         assert(strlen("hi") == 2);
@@ -118,6 +126,16 @@ void testDriver() {
         assert(!streq("test", "test2"));
         assert(!streq("test", "Test"));
     }
+    { // strcpy
+        char a[] = "hello";
+        char b[10] = "bye";
+        assert(strcmp(a, "hello") == 0);
+        assert(strcmp(b, "bye") == 0);
+        // copy to b from a
+        strcpy(b, a);
+        assert(strcmp(a, "hello") == 0);
+        assert(strcmp(b, "hello") == 0);
+    }
     {
          char str1[] = {'h', 'i', '\0', '!'};
          char str2[10] = "blah";
@@ -126,7 +144,110 @@ void testDriver() {
          assert(streq(str1, "hi"));
          assert(streq(str2, "hi"));
     }
+    { // copyCString
+        char a[] = "hello";
+        char b[10] = "bye";
+        assert(strcmp(a, "hello") == 0);
+        assert(strcmp(b, "bye") == 0);
+        // copy to b from a
+        copyCString(b, a);
+        assert(strcmp(a, "hello") == 0);
+        assert(strcmp(b, "hello") == 0);
+    }
+    { // examples of istringstream
+        int i;
+        // have person type 789 in the console
+        // cin >> i;
+        istringstream in("789");
+        in >> i;
+        assert(i == 789);
+    }
+    { // test readNext function
+        istringstream in("this is  a  test");
+        char* s = nextWord(in);
+        assert(s && strcmp(s, "this") == 0);
+        delete [] s;
+        s = nextWord(in);
+        assert(s && strcmp(s, "is") == 0);
+        delete [] s;
+        s = nextWord(in);
+        assert(s && strcmp(s, "a") == 0);
+        delete [] s;
+        s = nextWord(in);
+        assert(s && strcmp(s, "test") == 0);
+        delete [] s;
+        s = nextWord(in);
+        assert(s == nullptr);
+    }
+
+
     cout << "All tests passed." << endl;
+}
+
+char* nextWord(std::istream& input) {
+    // reserve some space; if we run out, we can get more space later
+    // and copy our str over
+    int capacity = 10;
+    char* str = new char[capacity + 1];
+    // read character by character, skipping all spaces (until we find
+    // the first non-space)
+    char nextChar;
+    // remember that input >> nextChar skips space
+    // just use input.get()
+    while (input >> nextChar && isspace(nextChar)) {
+        // skip the character
+    }
+    int stringLength = 0;
+    if (input) { // if we read at least one non-space character
+        if (stringLength >= capacity) {
+            // grow our string
+            // reserve twice as much space
+            capacity *= 2;
+            char* newSpace = new char[capacity + 1];
+            // copy the old stuff to the new space
+            copyCString(newSpace, str);
+            // delete the old space
+            delete [] str;
+            // track the new pointer
+            str = newSpace;
+        }
+        // add this to end of our string
+        str[stringLength++] = nextChar;
+        str[stringLength] = '\0';
+    }
+    // read character by character until we find the next space,
+    // adding non-spaces to our c-string
+    while (input >> nextChar && !isspace(nextChar)) {
+        if (stringLength >= capacity) {
+            // grow our string
+            // reserve twice as much space
+            capacity *= 2;
+            char* newSpace = new char[capacity + 1];
+            // copy the old stuff to the new space
+            copyCString(newSpace, str);
+            // delete the old space
+            delete [] str;
+            // track the new pointer
+            str = newSpace;
+        }
+        // add this to end of our string
+        str[stringLength++] = nextChar;
+        str[stringLength] = '\0';
+    }
+    // return the string
+    return str;
+}
+
+// 't', 'e', 's', 't', '\0'
+// 't', 'e', 's', 't', 
+void copyCString(char* dest, const char* src) {
+    // copy character by character from src[i] to dest[i] until src[i] == '\0'
+    int i;
+    for (i = 0; src[i]; i++) {
+        dest[i] = src[i];
+    }
+    // copy null char
+    dest[i] = src[i];
 }
 
 bool streq(const char* a, const char* b) {
@@ -145,10 +266,6 @@ int stringLength(const char* s) {
 int main() {
     testDriver();
     return 0;
-}
-
-void copyCString(char* dest, const char* src) {
- // TODO: implement this
 }
 
 int compareCStrings(const char* lhs, const char* rhs) {
